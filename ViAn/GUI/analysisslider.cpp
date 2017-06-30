b805656a1887475ca057ec710fd514634ea583e0
henpe071 <henper098@gmail.com>
2017-06-30 15:51:54 +0200

    Added the visual representation on the slider for POIs and added a "play pois only" option
    play pois only might not be the most effektive

--- ViAn/GUI/analysisslider.cpp
+++ ViAn/GUI/analysisslider.cpp
@@ -27,14 +27,22 @@ void AnalysisSlider::paintEvent(QPaintEvent *ev) {
     painter.drawComplexControl(QStyle::CC_Slider, option);
     QRect groove_rect = style()->subControlRect(QStyle::CC_Slider, &option, QStyle::SC_SliderGroove, this);
 
-    if (!rects.empty()) {
+    if (m_show_pois) {
         for (auto it = rects.begin(); it != rects.end(); ++it) {
-            QRect rect(groove_rect.left() + (*it).first * groove_rect.width(), groove_rect.top(), ((*it).second - (*it).first) * groove_rect.width(), groove_rect.height());
+            QRect rect(groove_rect.left() + (double)(*it).first * groove_rect.width() / maximum(),
+                       groove_rect.top(), ((double)(*it).second - (*it).first) * groove_rect.width() / maximum(), groove_rect.height());
             painter.fillRect(rect, QBrush(Qt::yellow));
         }
     }
     option.subControls = QStyle::SC_SliderHandle;
     painter.drawComplexControl(QStyle::CC_Slider, option);
+}
+
+void AnalysisSlider::set_analysis(Analysis* analysis) {
+    for (POI p : analysis->POIs) {
+        add_slider_interval(p.start_frame, p.end_frame);
+        std::cout << p.start_frame << " - " << p.end_frame << std::endl;
+    }
 }
 
 /**
@@ -45,7 +53,7 @@ void AnalysisSlider::paintEvent(QPaintEvent *ev) {
  * @param end frame
  */
 void AnalysisSlider::add_slider_interval(int start_frame, int end_frame) {
-    double first, second;
+    /*double first, second;
     first = (double)start_frame/maximum();
     second = (double)end_frame/maximum();
     std::pair<double, double> pair;
@@ -53,8 +61,60 @@ void AnalysisSlider::add_slider_interval(int start_frame, int end_frame) {
         pair = std::make_pair(second, first);
     } else {
         pair = std::make_pair(first, second);
+    }*/
+
+    std::pair<int, int> pair;
+    if (start_frame > end_frame) {
+        pair = std::make_pair(end_frame, start_frame);
+    } else {
+        pair = std::make_pair(start_frame, end_frame);
     }
     rects.push_back(pair);
+}
+
+int AnalysisSlider::get_next_poi_start(int frame) {
+    for (std::pair<int, int> rect : rects) {
+        if ( rect.first > frame) {
+            return rect.first;
+        }
+    }
+    return frame;
+}
+
+int AnalysisSlider::get_next_poi_end(int frame) {
+    for (std::pair<int, int> rect : rects) {
+        if ( rect.first > frame) {
+            return rect.second;
+        }
+    }
+    return frame;
+}
+
+int AnalysisSlider::get_prev_poi_start(int frame) {
+    int new_frame = frame;
+    for (std::pair<int, int> rect : rects) {
+        if ( rect.second > frame) {
+            break;
+        } else new_frame = rect.first;
+    }
+    return new_frame;
+}
+
+bool AnalysisSlider::is_in_POI(int frame) {
+    for (std::pair<int, int> rect : rects) {
+        if (frame > rect.first && frame < rect.second) {
+            return true;
+        }
+    }return false;
+}
+
+/**
+ * @brief AnalysisSlider::set_show_pois
+ * @param show_pois
+ */
+void AnalysisSlider::set_show_pois(bool show_pois) {
+    m_show_pois = show_pois;
+    repaint();
 }
 
 /**
