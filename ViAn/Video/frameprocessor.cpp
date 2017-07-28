4739638bf4394dd382fefa7a7d81ee1c2de80533
Fredrik Iselius <fredrik.iselius@gmail.com>
2017-07-28 16:54:07 +0200

    Playback is now seperated into two threads. One that reads the video file and produces frames and another one that processes them.

--- ViAn/Video/frameprocessor.cpp
+++ ViAn/Video/frameprocessor.cpp
@@ -2,18 +2,20 @@
 #include <QDebug>
 #include <QTime>
 
-FrameProcessor::FrameProcessor() {}
+FrameProcessor::FrameProcessor(std::atomic_bool* new_frame, std::atomic_bool* changed,
+                               zoomer_settings* z_settings, std::atomic_int* width, std::atomic_int* height,
+                               std::atomic_bool* new_video, manipulation_settings* m_settings, video_sync* v_sync) {
+    m_new_frame = new_frame;
 
-/**
- * @brief FrameProcessor::on_new_frame
- * Slot function for recieving a new frame
- * @param frame : mat object containing the frame
- * @param frame_index   : index of the frame
- */
-void FrameProcessor::on_new_frame(cv::Mat frame, int frame_index) {
-    m_frame = frame.clone();
-    m_frame_index = frame_index;
-    process_frame();
+    m_changed = changed;
+    m_z_settings = z_settings;
+    m_man_settings = m_settings;
+    m_v_sync = v_sync;
+    m_new_video = new_video;
+
+    m_width = width;
+    m_height = height;
+    cv::namedWindow("test");
 }
 
 /**
@@ -113,6 +115,58 @@ void FrameProcessor::on_move_zoom_rect(int x, int y) {
 }
 
 /**
+ * @brief FrameProcessor::check_events
+ * This method handles all processing requests and will sleep when there is none.
+ * It uses a conditional variable and waits for one of three bools (m_new_frame, m_changed, m_new_video).
+ * Each bool will trigger a different behavior:
+ * m_new_frame: copies and processes the current frame. This is done during ordinary playback
+ * m_changed: indicates that one of the processing settings has been changed by the user and must be applied
+ * m_new_video: a new video has been loaded, processing settings are restored
+ */
+void FrameProcessor::check_events() {
+    while (true) {
+        std::unique_lock<std::mutex> lk(m_v_sync->lock);
+        m_v_sync->con_var.wait(lk, [&]{return m_new_frame->load() || m_changed->load() || m_new_video->load();});
+
+        // Settings has been changed by the user
+        if (m_changed->load()) {
+            qDebug("here");
+            m_changed->store(false);
+
+            update_zoomer_settings();
+            update_manipulator_settings();
+
+            // Skip reprocessing of old frame if there is a new
+            if (!m_new_frame->load()) {
+                qDebug("Processing with new settings");
+                process_frame();
+                lk.unlock();
+                continue;
+            }
+        }
+
+        // A new frame has been loaded by the VideoPlayer
+        if (m_new_frame->load()) {
+            m_new_frame->store(false);
+
+            m_frame = m_v_sync->frame.clone();
+            process_frame();
+
+            lk.unlock();
+            m_v_sync->con_var.notify_one();
+            continue;
+        }
+
+        // A new video has been loaded. Reset processing settings
+        if (m_new_video->load()) {        
+            reset_settings();
+        }
+        lk.unlock();
+
+    }
+}
+
+/**
  * @brief FrameProcessor::process_frame
  * Applies various manipulations to the frame currently held by the class.
  * These manipulations consists of rotation, zoom and brightness/contrast changes.
@@ -121,23 +175,121 @@ void FrameProcessor::on_move_zoom_rect(int x, int y) {
 void FrameProcessor::process_frame() {
 
     if (m_frame.empty()) return;
-
     cv::Mat manipulated_frame = m_frame.clone();
-//    processed_frame = analysis_overlay->draw_overlay(processed_frame, get_current_frame_num());
-//    processed_frame = video_overlay->draw_overlay(processed_frame, get_current_frame_num());
 
     // Rotates the frame, according to the choosen direction.
-    // If direction is in the valid range the frame is rotated.
     if (ROTATE_MIN <= m_rotate_direction && m_rotate_direction <= ROTATE_MAX) {
         cv::rotate(manipulated_frame, manipulated_frame, m_rotate_direction);
     }
 
+    // Displays the zoom rectangle on the original frame in a new windows.
+    // NIKLAS
+    cv::Mat tmp = manipulated_frame.clone();
+    cv::rectangle(tmp, m_zoomer.get_zoom_rect(), cv::Scalar(255,0,0));
+    imshow("test", tmp);
+
     // Scales the frame
-    if (m_zoomer.get_scale_factor() != 1) m_zoomer.scale_frame(manipulated_frame);
+    m_zoomer.scale_frame(manipulated_frame);
 
     // Applies brightness and contrast
     m_manipulator.apply(manipulated_frame);
 
     // Emit manipulated frame and current frame number
     done_processing(manipulated_frame, m_frame_index);
+}
+
+/**
+ * @brief FrameProcessor::update_zoomer_settings
+ * Updates the settings for the zoomer.
+ * Modifies data from a shared pointer and should only be called after the frame_lock has been acquired
+ */
+void FrameProcessor::update_zoomer_settings() {
+    // Viewport has changed size
+    if (m_z_settings->draw_area_size != m_zoomer.get_viewport_size()) {
+
+        m_zoomer.set_viewport_size(m_z_settings->draw_area_size);
+        m_zoomer.update_rect_size();
+    }
+    // Scale/zoom factor has been changed
+    else if (m_zoomer.get_scale_factor() != m_z_settings->zoom_factor) {
+        m_zoomer.set_scale_factor(m_z_settings->zoom_factor);
+    }
+    // Zoom rectangle has changed
+    else if (m_zoomer.get_zoom_rect() != cv::Rect(m_z_settings->zoom_tl.x(), m_z_settings->zoom_tl.y(),
+                                                    m_z_settings->zoom_br.x(), m_z_settings->zoom_br.y())) {
+
+        m_zoomer.set_zoom_rect(m_z_settings->zoom_tl, m_z_settings->zoom_br);
+    }
+    // Panning occured
+    else if (m_z_settings->x_movement != 0 || m_z_settings->y_movement != 0) {
+        m_zoomer.move_zoom_rect(m_z_settings->x_movement, m_z_settings->y_movement);
+        m_z_settings->x_movement = 0;
+        m_z_settings->y_movement = 0;
+    }
+    // Fit to screen
+    else if (m_z_settings->fit) {
+        m_z_settings->fit = false;
+        m_zoomer.fit_viewport();
+    }
+    // Set original size (no zoom)
+    else if (m_z_settings->original){
+        m_z_settings->original = false;
+        m_zoomer.set_scale_factor(1);
+    }
+
+    // Store changes made
+    m_z_settings->zoom_factor = m_zoomer.get_scale_factor();
+    cv::Rect tmp = m_zoomer.get_zoom_rect();
+    m_z_settings->zoom_tl = QPoint(tmp.x, tmp.y);
+    m_z_settings->zoom_br = QPoint(tmp.width, tmp.height);
+}
+
+/**
+ * @brief FrameProcessor::update_manipulator_settings
+ * Updates manipulator and rotation settings.
+ * Modifies data from a shared pointer and should therefor only be used after
+ * the frame_lock has been acquired.
+ */
+void FrameProcessor::update_manipulator_settings() {
+    m_manipulator.set_brightness(m_man_settings->brightness);
+    m_manipulator.set_contrast(m_man_settings->contrast);
+
+    if (m_man_settings->rotate == 1) {
+        m_rotate_direction = (m_rotate_direction + 1) % ROTATE_NUM;
+        m_zoomer.flip();
+    } else if (m_man_settings->rotate == -1) {
+        m_rotate_direction = (m_rotate_direction + (ROTATE_NUM - 1)) % ROTATE_NUM;
+        m_zoomer.flip();
+    }
+    m_man_settings->rotate = 0;
+}
+
+
+/**
+ * @brief FrameProcessor::reset_settings
+ * Resets the processing settings.
+ * Should be used after a new video has been loaded by the VideoPlayer.
+ * (Modifies shared data. Use frame_lock)
+ */
+void FrameProcessor::reset_settings() {
+    m_new_video->store(false);
+
+    m_rotate_direction = ROTATE_NONE;
+
+    // Reset manipulator values
+    m_manipulator.reset();
+    m_man_settings->brightness = m_manipulator.BRIGHTNESS_DEFAULT;
+    m_man_settings->contrast = m_manipulator.CONTRAST_DEFAULT;
+
+    m_zoomer.set_frame_size(cv::Size(m_width->load(), m_height->load()));
+    m_zoomer.reset();
+    // Centers zoom rectangle and displays the frame without zoom
+//    m_zoomer.set_scale_factor(1);
+    m_zoomer.fit_viewport();
+
+    // Store current zoomer settings to shared structure
+    m_z_settings->zoom_factor = m_zoomer.get_scale_factor();
+    cv::Rect tmp = m_zoomer.get_zoom_rect();
+    m_z_settings->zoom_tl = QPoint(tmp.x, tmp.y);
+    m_z_settings->zoom_br = QPoint(tmp.width, tmp.height);
 }
